fabf1af539d3549c0591542174c1551be249cc0f
Taher Ghasimakbari <tghasi@login03.msi.umn.edu>
2015-04-13 12:54:26 -0500

    The Structure Factor Name now includes a "."! Yay :)

--- src/inter/external/PeriodicExternal.h
+++ src/inter/external/PeriodicExternal.h
@@ -143,7 +143,7 @@ namespace Inter
    private:
    
       /// Maximum allowed value for nAtomType (# of particle types).
-      static const int MaxAtomType = 3;
+      static const int MaxAtomType = 4;
 
       /// Prefactor array ofsize nAtomType.
       DArray<double> prefactor_;
