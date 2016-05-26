6ab685243b05689f605cdccf9dbdb99cc2667235
TaherGhasimakbari <ghasi002@umn.edu>
2016-05-26 18:26:03 -0500

    Ramper implemented

---
+++ src/ddMd/modifiers/Ramper.cpp
@@ -0,0 +1,108 @@
+/*
+* Simpatico - Simulation Package for Polymeric and Molecular Liquids
+*
+* Copyright 2010 - 2014, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include "Ramper.h"
+#include <util/format/Int.h>
+#include <util/format/Dbl.h>
+#include <util/mpi/MpiLoader.h>
+#include <util/misc/ioUtil.h>
+
+#include <sstream>
+
+namespace DdMd
+{
+
+   using namespace Util;
+
+   /*
+   * Constructor.
+   */
+   Ramper::Ramper(Simulation& simulation)
+    : Modifier(simulation)
+   {set(Flags::PreIntegrate1);
+    setClassName("Ramper");}
+
+   /*
+   * Destructor.
+   */
+   Ramper::~Ramper()
+   {}
+
+   /*
+   * Read interval and outputFileName. 
+   */
+   void Ramper::readParameters(std::istream& in)
+   {
+      // Read interval value (inherited from Interval)
+      readInterval(in);
+      read<std::string>(in, "outputFileName", outputFileName_);
+      read<double>(in,"epsilon1", epsilon1_);
+      read<double>(in,"epsilonSlope", epsilonSlope_);
+
+      isInitialized_ = true;
+   }
+
+   /*
+   * Load internal state from an archive.
+   */
+   void Ramper::loadParameters(Serializable::IArchive &ar)
+   {
+      loadParameter<std::string>(ar, "outputFileName", outputFileName_);
+      loadParameter(ar,"epsilon1", epsilon1_);
+      loadParameter(ar,"epsilonSlope", epsilonSlope_);
+
+      isInitialized_ = true;
+   }
+
+   /*
+   * Save internal state to an archive.
+   */
+   void Ramper::save(Serializable::OArchive &ar)
+   {
+      ar << outputFileName_;
+   }
+
+   /*
+   * Get the outputFileName string with an added suffix
+   */
+   std::string Ramper::outputFileName(const std::string& suffix) const
+   {
+      std::string filename = outputFileName_;
+      filename += suffix;
+      return filename;
+   }
+
+   /*
+   * Set actual number of molecules and clear accumulator.
+   */
+   void Ramper::setup()
+   {
+      if (!isInitialized_) {
+         UTIL_THROW("Object not initialized.");
+      }
+   }
+
+   /*
+    * Save internal state to an archive.
+   */
+   void Ramper::preIntegrate1(long iStep)
+   {
+      if (isAtInterval(iStep)) {
+
+         double epsilon = epsilon1_ + epsilonSlope_*iStep;
+         simulation().pairPotential().set("epsilon", 0, 1, epsilon);
+         
+         if (simulation().domain().isMaster()) {
+
+            simulation().fileMaster().openOutputFile(outputFileName(".dat"), outputFile_);
+            outputFile_ << Int(iStep, 10) << Dbl(epsilon, 10) << std::endl;
+            outputFile_.close();
+         }
+      }
+   }
+
+}
---
+++ src/ddMd/modifiers/Ramper.h
@@ -0,0 +1,122 @@
+#ifndef DDMD_RAMPER_H
+#define DDMD_RAMPER_H
+
+/*
+* Simpatico - Simulation Package for Polymeric and Molecular Liquids
+*
+* Copyright 2010 - 2014, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include <ddMd/modifiers/Modifier.h>        // base class
+#include <util/misc/FileMaster.h>           // member variable
+#include <ddMd/simulation/Simulation.h>
+
+#ifndef DDMD_NOPAIR
+#include <ddMd/potentials/pair/PairPotential.h>
+#include <ddMd/potentials/pair/PairFactory.h>
+#endif
+
+namespace DdMd
+{
+
+   using namespace Util;
+
+   /**
+   * The design of the Ramper class was inspired by the Lammps "Fix" 
+   * base class, which provides a very flexible framework for designing
+   * algorithms that can modify the state of a system.
+   *
+   * \ingroup DdMd_Ramper_Module
+   */
+   class Ramper : public Modifier
+   {
+
+   public:
+
+      /**
+      * Default constructor (for unit testing)
+      */
+      Ramper();
+
+      /**
+      * Constructor (for use in simulation).
+      */
+      Ramper(Simulation& simulation);
+
+      /**
+      * Destructor.
+      */
+      virtual ~Ramper();
+
+      /**
+      * Read dumpPrefix and interval.
+      *
+      * \param in input parameter file
+      */
+      virtual void readParameters(std::istream& in);
+
+      /**
+      * Load internal state from an archive.
+      *
+      * \param ar input/loading archive
+      */
+      virtual void loadParameters(Serializable::IArchive &ar);
+
+      /**
+      * Save internal state to an archive.
+      *
+      * \param ar output/saving archive
+      */
+      virtual void save(Serializable::OArchive &ar);
+
+      /**
+      * Setup before entering the main loop.
+      */ 
+      virtual void setup();
+
+      /// \name Integration action functions
+      //@{ 
+ 
+      /** 
+      * Call just before the first step of velocity-Verlet algorithm. 
+      *
+      * Atom positions are Cartesian on entry and return.
+      */
+      virtual void preIntegrate1(long iStep);
+
+      /** 
+      * Call just before the first step of velocity-Verlet algorithm. 
+      *
+      * Atom positions are Cartesian on entry and return.
+      */
+      virtual void output();
+
+      protected:
+
+      /**
+      * Return outputFileName string with added suffix.
+      */
+      std::string outputFileName(const std::string& suffix) const;
+
+      private:
+
+      /// Output file stream
+      std::ofstream  outputFile_;
+
+      /// Base name of output file(s).
+      std::string outputFileName_;
+
+      /// Stress Measurement Interval
+      double  epsilon1_;
+
+      /// Stress Measurement Interval
+      double  epsilonSlope_;
+
+      /// Has readParam been called?
+      long  isInitialized_;
+
+   };
+
+}
+#endif
